cae56307a4cbb123a7809b4e70086a21b981311b
PauloMonroyC <pmonroycotrina@byupathway.edu>
2026-09-05 00:06:24 -0500

    Excercise1 completed

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("This is in C#");
+
+
+
+
+
+        Console.WriteLine("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}");
+
     }
 }
