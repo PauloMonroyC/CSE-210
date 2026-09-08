6e425ff62b91043544663707289356c6b2dd4029
PauloMonroyC <pmonroycotrina@byupathway.edu>
2026-09-07 22:33:40 -0500

    excercise 3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+
+        
+        
+
+        Random randomGenerator = new Random(); int magicNumber =
+        randomGenerator.Next(1,101);
+
+        Console.WriteLine("Try to guess the magic number");
+
+        int guess;
+
+        do
+        {
+            Console.WriteLine("What is your guess?");
+            string number2 = Console.ReadLine();
+            guess = int.Parse(number2);
+
+            if (guess == magicNumber)
+            {
+            Console.WriteLine("You guessed it!");
+            }
+            else if (guess < magicNumber)
+            {
+            Console.WriteLine("Higher");
+            }
+            else
+            {
+            Console.WriteLine("Lower");
+            }
+
+        } while (guess != magicNumber);
+        
+        
     }
 }
