5aa1c144b50b9daab7bd0a6b2e61abb31482eaeb
PauloMonroyC <pmonroycotrina@byupathway.edu>
2026-09-24 00:49:14 -0500

    Learning activitiy completed

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+        
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFranctionString());
+        Console.WriteLine(f1.GerDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFranctionString());
+        Console.WriteLine(f2.GerDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFranctionString());
+        Console.WriteLine(f3.GerDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFranctionString());
+        Console.WriteLine(f4.GerDecimalValue());
+
+        
     }
 }
